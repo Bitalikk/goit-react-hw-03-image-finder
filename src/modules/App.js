32ddc90b7d16dd8084a8e550775768aefe50769b
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2019-06-23 14:03:37 +0300

    new project

---
+++ src/modules/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      {console.log('wass')}
+    </div>
+  );
+}
+
+export default App;
